d74a1966c8583a968ba9b93a40bbdede548e8de5
mashho <mashhooddanish@gmail.com>
2025-04-10 19:43:15 +0530

    Adding two files

---
+++ cart.js
@@ -0,0 +1,12 @@
+const cartItems = [
+    {
+        name:"Iphone",
+        quantity:1,
+        discount:1000
+    },
+    {
+        name:"Washing Machine",
+        quantity:1,
+        discount:499
+    }
+]
---
+++ product.js
@@ -0,0 +1,22 @@
+const productList = [
+    {
+        name:"Iphone",
+        quantity:1,
+        discount:1000
+    },
+    {
+        name:"Washing Machine",
+        quantity:1,
+        discount:499
+    },
+    {
+        name:"Lights",
+        quantity:2,
+        discount:0
+    },
+    {
+        name:"Laptop",
+        quantity:1,
+        discount:2999
+    }
+]
